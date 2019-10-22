06c003ea91a0ba23e278f36afe29bc3daa30140e
bonbonpapa <dui.zhang@gmail.com>
2019-10-22 17:45:08 -0400

    add button fuction

--- public/app.js
+++ public/app.js
@@ -1,7 +1,11 @@
 let body = document.querySelector('body')
 let lost = false
 let won = false
-let clicked = [false, false]
+// let clicked = [false, false]
+let clicked = []
+let getRndInteger = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 let first = document.createElement('button')
 first.innerText = "one"
 first.addEventListener('click', () => {
@@ -12,10 +16,37 @@ first.addEventListener('click', () => {
         document.getElementById('status').innerText = "you won!"
     }
 })
-body.appendChild(second)
-setTimeout( ()=> {
-    if(won || lost) return
-    lost = true
-    document.getElementById('status').innerText = "you lost!"
-},1000)
+body.appendChild(first) 
+let second = document.createElement('button') 
+second.innerText = "two" 
+second.addEventListener('click', () => { 
+    if (won || lost) return 
+    clicked[1] = true 
+    if (clicked[0] && clicked[1]) { 
+        won = true 
+        document.getElementById('status').innerText = "you won!" 
+    } 
+}) 
+body.appendChild(second) 
+// add the function to add the button and update the clicked array
+let addButton = (str) => {
+    let btObj = document.createElement('button')
+    btObj.innerText = str
+    btObj.addEventListener('click', () => {
+    if (won || lost) return
+    clicked.push(true)
+    
+    if (clicked.every((clik)=> {return clik})) {
+        won = true
+        document.getElementById('status').innerText = "you won!"
+    }
+    })
+    body.appendChild(btObj) 
 
+}
+
+setTimeout(() => { 
+    if (won || lost) return 
+    lost = true 
+    document.getElementById('status').innerText = "You lost!" 
+}, getRndInteger(500, 1300)) 
