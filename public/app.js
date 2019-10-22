cbb27d108b64a1f31f9e428b666c2ce245bce98c
bonbonpapa <dui.zhang@gmail.com>
2019-10-22 17:58:25 -0400

    add button function implemented

--- public/app.js
+++ public/app.js
@@ -6,37 +6,41 @@ let clicked = []
 let getRndInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-let first = document.createElement('button')
-first.innerText = "one"
-first.addEventListener('click', () => {
-    if (won || lost) return
-    clicked[0] = true
-    if (clicked[0] && clicked[1]) {
-        won = true
-        document.getElementById('status').innerText = "you won!"
-    }
-})
-body.appendChild(first) 
-let second = document.createElement('button') 
-second.innerText = "two" 
-second.addEventListener('click', () => { 
-    if (won || lost) return 
-    clicked[1] = true 
-    if (clicked[0] && clicked[1]) { 
-        won = true 
-        document.getElementById('status').innerText = "you won!" 
-    } 
-}) 
-body.appendChild(second) 
+// let first = document.createElement('button')
+// first.innerText = "one"
+// first.addEventListener('click', () => {
+//     if (won || lost) return
+//     clicked[0] = true
+//     if (clicked[0] && clicked[1]) {
+//         won = true
+//         document.getElementById('status').innerText = "you won!"
+//     }
+// })
+// body.appendChild(first) 
+// let second = document.createElement('button') 
+// second.innerText = "two" 
+// second.addEventListener('click', () => { 
+//     if (won || lost) return 
+//     clicked[1] = true 
+//     if (clicked[0] && clicked[1]) { 
+//         won = true 
+//         document.getElementById('status').innerText = "you won!" 
+//     } 
+// }) 
+// body.appendChild(second) 
+
+
 // add the function to add the button and update the clicked array
 let addButton = (str) => {
     let btObj = document.createElement('button')
     btObj.innerText = str
+    clicked.push(false)
     btObj.addEventListener('click', () => {
     if (won || lost) return
-    clicked.push(true)
     
-    if (clicked.every((clik)=> {return clik})) {
+    clicked.pop()
+
+    if (clicked.length === 0) {
         won = true
         document.getElementById('status').innerText = "you won!"
     }
@@ -45,8 +49,12 @@ let addButton = (str) => {
 
 }
 
+addButton('one')
+addButton('two')
+addButton('three')
+
 setTimeout(() => { 
     if (won || lost) return 
     lost = true 
     document.getElementById('status').innerText = "You lost!" 
-}, getRndInteger(500, 1300)) 
+}, getRndInteger(1000, 2000)) 
